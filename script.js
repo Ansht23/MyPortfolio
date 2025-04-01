b4fe16b6914a07bd742cd2ae5e8c75d60287c606
Ansht23 <anshtiwari231102@gmail.com>
2025-04-01 18:04:17 +0530

    Responsive0

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
   document.getElementById("nav-list").classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu");
+  const navList = document.querySelector("nav ul");
+
+  menuIcon.addEventListener("click", function () {
+    navList.classList.toggle("active");
+  });
+});
